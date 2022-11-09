7788d04603bb08ee75bf2625285de984ee65afb7
ritvikbanakar <ritvikbanakar15@gmail.com>
2022-11-08 19:30:47 -0600

    Set up telescope

--- nvim/.config/nvim/lua/ritvikbanakar/packer.lua
+++ nvim/.config/nvim/lua/ritvikbanakar/packer.lua
@@ -13,5 +13,10 @@ return require("packer").startup(function(use)
             ts_update()
         end,
     }
+    use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
 end)
   
